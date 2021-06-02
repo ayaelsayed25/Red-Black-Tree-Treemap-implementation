c260a99ccab9daeb6840533d7bd1469b37f52878
Radwa Ahmed <52842936+radwaahmed20112000@users.noreply.github.com>
2021-06-02 19:09:58 +0200

    Interfaces

---
+++ src/RedBlackTree.java
@@ -0,0 +1,39 @@
+import Interfaces.INode;
+import Interfaces.IRedBlackTree;
+
+public class RedBlackTree implements IRedBlackTree {
+    @Override
+    public INode getRoot() {
+        return null;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public Object search(Comparable key) {
+        return null;
+    }
+
+    @Override
+    public boolean contains(Comparable key) {
+        return false;
+    }
+
+    @Override
+    public void insert(Comparable key, Object value) {
+
+    }
+
+    @Override
+    public boolean delete(Comparable key) {
+        return false;
+    }
+}
