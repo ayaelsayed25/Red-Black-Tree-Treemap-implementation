97f7fb387f440175a7050fb2eda90e96157f3bda
sarayasser-coder <sara_yasserii@yahoo.com>
2021-06-06 19:50:40 +0200

    Node

--- src/Node.java
+++ src/Node.java
@@ -1,70 +1,74 @@
 import Interfaces.INode;
 
 public class Node<T extends Comparable<T>, V> implements INode {
+
+        private T key;
+        private V value;
+        private INode<T,V> leftChild, rightChild,parent;
+        private boolean color;
+
     @Override
     public void setParent(INode parent) {
-
+        this.parent = parent;
     }
 
     @Override
     public INode getParent() {
-        return null;
+        return this.parent;
     }
 
     @Override
     public void setLeftChild(INode leftChild) {
-
+        this.leftChild = leftChild;
     }
 
     @Override
     public INode getLeftChild() {
-        return null;
+        return this.leftChild;
     }
 
     @Override
     public void setRightChild(INode rightChild) {
-
+        this.rightChild = rightChild;
     }
 
     @Override
     public INode getRightChild() {
-        return null;
+        return this.rightChild;
     }
 
     @Override
-    public Comparable<T> getKey() {
-        return null;
+    public Comparable getKey() {
+        return this.key;
     }
 
     @Override
     public void setKey(Comparable key) {
-
+        this.key = (T) key;
     }
 
     @Override
     public Object getValue() {
-        return null;
+        return this.value;
     }
 
     @Override
     public void setValue(Object value) {
-
+        this.value = (V) value;
     }
 
     @Override
     public boolean getColor() {
-        return false;
+        return this.color;
     }
 
     @Override
     public void setColor(boolean color) {
-
+        this.color = color;
     }
 
     @Override
     public boolean isNull() {
-        return false;
+        return this.key == null;
     }
-
-
 }
